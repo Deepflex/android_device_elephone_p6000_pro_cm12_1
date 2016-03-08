5d7c6419a87c39d0841e7dee75dd6048faa950ae
Deepflex <saddist42@gmail.com>
2016-03-08 11:40:40 +0700

    Stable #02
    Stable #02

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,16 +39,17 @@ TARGET_CPU_CORTEX_A53 := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_NO_FACTORYIMAGE := true
  
-TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI),$(TARGET_CPU_ABI2)
+TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
 TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
-TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT)$(TARGET_CPU_ABI_LIST_32_BIT)
+TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
  
 TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
 TARGET_USERIMAGES_USE_EXT4:=true
- 
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
  
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
@@ -66,19 +67,20 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 444596224
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --tags_offset 0x0df88000
  
-#in case we want to build kernel from source
-#TARGET_KERNEL_SOURCE := kernel/elephone/p6000_pro
-#TARGET_KERNEL_CONFIG := cyanogenmod_p6000_pro_defconfig
+# build kernel from source
+# TARGET_KERNEL_SOURCE := kernel/elephone/p6000_pro
+# TARGET_KERNEL_ARCH := arm64
+# TARGET_KERNEL_HEADER_ARCH := arm64
+# TARGET_KERNEL_CONFIG := cyanogenmod_p6000_pro_defconfig
+# TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+# MTK_APPENDED_DTB_SUPPORT := yes
+# BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
  
-#for now lets use prebuilt
+# prebuild kernel as fallback
 TARGET_PREBUILT_KERNEL := device/elephone/p6000_pro/prebuilt/kernel
 BOARD_HAS_NO_SELECT_BUTTON := true
-#recovery
-#TARGET_RECOVERY_INITRC := device/elephone/p6000_pro/recovery/init.mt6753.rc
-TARGET_RECOVERY_FSTAB := device/elephone/p6000_pro/recovery/root/fstab.mt6753
-TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness\"
 
-#system.prop
+# system.prop
 TARGET_SYSTEM_PROP := device/elephone/p6000_pro/system.prop
 
 # WiFi
@@ -114,6 +116,21 @@ BOARD_USES_MTK_AUDIO := true
 MTK_FM_SUPPORT :=true
 MTK_FM_RX_SUPPORT :=true
 
+#Mediatek flags
+BOARD_HAS_MTK_HARDWARE := true
+MTK_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DMTK_HARDWARE -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
+COMMON_GLOBAL_CPPFLAGS += -DMTK_HARDWARE
+
+#EGL settings
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/elephone/p6000_pro/egl.cfg
+
+# recovery
+#TARGET_RECOVERY_INITRC := device/elephone/p6000_pro/recovery/init.mt6753.rc
+TARGET_RECOVERY_FSTAB := device/elephone/p6000_pro/recovery/root/fstab.mt6753
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness\"
+
 #twrp ( WIP do not use!!! see comments )
 
 #tw_theme is essential flag
@@ -146,17 +163,6 @@ TW_MAX_BRIGHTNESS := 255
 
 #F2FS support (only activate if kernel supports)
 #TARGET_USERIMAGES_USE_F2FS:=true
-
-
-#Mediatek flags
-BOARD_HAS_MTK_HARDWARE := true
-MTK_HARDWARE := true
-COMMON_GLOBAL_CFLAGS += -DMTK_HARDWARE -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
-COMMON_GLOBAL_CPPFLAGS += -DMTK_HARDWARE
-
-#EGL settings
-USE_OPENGL_RENDERER := true
-BOARD_EGL_CFG := device/elephone/p6000_pro/egl.cfg
 
 # Hack for build
 $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,6 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Elephone p6000_pro"
+#define BTM_DEF_LOCAL_NAME   "Elephone P6000 Pro"
 
 #endif
